241985cc4808aacbde2032e9f41e3f0a91f36b2c
CameronOwens <camowens58@gmail.com>
2023-01-20 09:19:24 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] games = { "Madden", "NBA2K", "God of War", "Call of Duty", "Mario Kart" };
+
+            IEnumerable<string> gameOrder = games.OrderBy(str => str.Length).ThenBy(str => str);
+
+            foreach (string game in gameOrder)
+                Console.WriteLine(game);
+
+            Console.ReadLine();
+            
         }
     }
 }
